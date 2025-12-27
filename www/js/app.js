95cc7ce726739ac43fb6ea5a421731417dd03a98
cyrusphipps <cyrus.phipps@gmail.com>
2025-12-27 14:35:56 -0700

    Update deviceready log to 1.0.2

--- www/js/app.js
+++ www/js/app.js
@@ -75,7 +75,7 @@ function initLimetunaPortal() {
 
 // Cordova deviceready handling
 function onDeviceReady() {
-  console.log("Cordova deviceready fired, initializing Gulfhit 1.0.1 portal");
+  console.log("Cordova deviceready fired, initializing Gulfhit 1.0.2 portal");
   initLimetunaPortal();
 }
 
