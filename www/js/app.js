cb5039eed906ea3310d9aef79b443915c71b1b9c
cyrusphipps <cyrus.phipps@gmail.com>
2026-01-07 11:05:02 -0700

    Adjust correct sound overlap and bump version

--- www/js/app.js
+++ www/js/app.js
@@ -106,7 +106,7 @@ function initLimetunaPortal() {
 
 // Cordova deviceready handling
 function onDeviceReady() {
-  console.log("Cordova deviceready fired, initializing Gulfhit 1.6.9 portal");
+  console.log("Cordova deviceready fired, initializing Gulfhit 1.6.10 portal");
   initLimetunaPortal();
 }
 
