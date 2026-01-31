bb9f53fbda039da44acb42c8206845bfecc988e7
cyrusphipps <cyrus.phipps@gmail.com>
2026-01-30 23:23:57 -0700

    Update app version and intro padding

--- www/js/app.js
+++ www/js/app.js
@@ -144,7 +144,7 @@ function resetAnimalsProgress() {
 
 // Cordova deviceready handling
 function onDeviceReady() {
-  console.log("Cordova deviceready fired, initializing Gulfhit 2.0.11 portal");
+  console.log("Cordova deviceready fired, initializing Gulfhit 2.0.12 portal");
   initLimetunaPortal();
 }
 
